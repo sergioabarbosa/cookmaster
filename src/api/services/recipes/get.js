c10e778020c968548e0985d5aede45469bb3bc00
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-12-03 09:45:36 -0300

    Desafio 6

--- src/api/services/recipes/get.js
+++ src/api/services/recipes/get.js
@@ -2,7 +2,6 @@ const Get = require('../../models/recipes/get');
 
 module.exports = async (id) => {
   const result = await Get('recipes', id); 
-  console.log(result);
   if (result.length) return { code: 200, body: result[0] };
 
   return { code: 404, body: { message: 'recipe not found' } };
